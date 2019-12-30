00a8383a1370b57a52bbc3b547acab463a22df34
heikoliedtke <heiko.liedtke@dimensiondata.com>
2019-12-30 17:53:53 +0100

    small changes

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app.secret_key = "Kommernetkrumm"
 @app.route('/')
 def entry_page() -> 'html':
     myhost = os.uname()
-    return render_template ("base.html", the_host=myhost[1], the_title="Demo Web App")
+    return render_template ("base.html", the_host=myhost[1], the_sysname=myhost[0], the_version=myhost[3],
+     the_title="Demo Web App")
 
 
 
